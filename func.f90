4ef93a7833506d5da20734e2e5704ea49ee5a3ee
greatcactus <kaiinntourokuyouno.1@gmail.com>
2021-05-02 14:27:35 +0900

    ADD: added primitive functions

--- func.f90
+++ func.f90
@@ -1,50 +1,86 @@
 module func
-implicit none
-public
-    real(8), save, private:: eps, a, d, h
+    implicit none
+    public
+    real(8), save, private:: eps = 0.0125, a = 2., d = 4.5, h = 45., c = 5.
     logical, save, public :: isConstRead = .false.
 
 contains
 
 logical function const_read()
     const_read = isConstRead
-end function
+end function const_read
 
 !##
 !.... Here are the functions used for computation
-real(8) function k(u, j, j)
-    real(8):: u
-    integer:: i, j
+real(8) function k(u_ij)
+    real(8):: u_ij
 
-    k = exp( u(i,j) / (1 + u(j,j)/c) )
-end function
+    k = exp( u_ij / (1 + u_ij/c) )
+end function k
 
-real(8) function f(u, v, i, j)
-    real(8):: u, v
-    integer:: i, j
+real(8) function f(u_ij, v_ij)
+    real(8):: u_ij, v_ij
 
-    f = 1./eps * ( -a*u(i,j) + k( u(i,j) )*v(i,j) )
-end function
+    f = 1./eps * ( -a*u_ij + k( u_ij )*v_ij )
+end function f
 
-real(8) function g(u, v, i, j)
-    real(8):: u, v
-    integer:: i, j
+real(8) function g(u_ij, v_ij)
+    real(8):: u_ij, v_ij
 
-    g = h*( 1-v(i,j) ) -k( u(i,j) ) * v(i,j)
-end function
+    g = h*( 1-v_ij ) -k( u_ij ) * v_ij
+end function g
 
-real(8) laplacian(f, i, j, di, dj)
-    real(8):: f
+real(8) function laplacian(fi_bw, fij, fi_fw, fj_bw, fj_fw, di, dj)
+    real(8):: fi_bw, fij, fi_fw, fj_bw, fj_fw
     real(8):: di, dj
-    integer:: i, j
 
-    laplacian = ( f(i+1,j)-2*f(i,j)+f(i-1,j) )/(di*di)&
-    &         + ( f(i,j+1)-2*f(i,j)+f(j,i-1) )/(dj*dj)
-end function
+    laplacian = ( fi_fw- 2*fij + fi_bw )/(di*di)&
+    &         + ( fj_fw- 2*fij + fj_bw )/(dj*dj)
+end function laplacian
 !.... end
 !##
 
+!##
+!.... Here is the functions can be used for simple coding
+function linspace(x0, x1, nd) result(x)
+!.... Makes array of nx number of points
+    real(8), intent(in):: x0, x1
+    integer, optional, intent(in):: nd
+    real(8), dimension(:), allocatable:: x
+    integer:: n, i, ierr
+    real(8):: dx
 
+    if ( present(nd) ) then
+        n = nd
+        if (n < 1) then
+            call stop_error("meshexp: nx > 1 required.")
+        endif
+    else
+        n=100!    Default n
+    endif
 
+    allocate(x(n))
+    if (n == 1) then
+        x(1) = x1
+        return
+    endif
+
+    dx = (x1-x0)/dble(n-1)
+    do i=1, n
+        x(i) = x0 + dx*dble(i-1)
+    enddo
+end function linspace
+
+subroutine stop_error(msg)
+! Aborts the program with nonzero exit code
+! stop_error() uses the statement "stop 1" which returns an exit code
+! 1 and prints the messeage.
+
+    character(len=*):: msg
+    print *, msg
+    stop 1
+end subroutine stop_error
+!.... end
+!##
 
 end module func
--- main.f90
+++ main.f90
@@ -1,3 +1,19 @@
 program main
+    use func
+    implicit none
+    integer:: ngrid=11
+    integer:: dt
+    real(8):: xlen=10., ylen=10.
+    real(8), allocatable, dimension(:,:):: U, V
+    real(8), allocatable, dimension(:)  :: X, Y
 
-end program
+    allocate( X(ngrid) )
+    allocate( Y(ngrid) )
+    allocate( U(ngrid, ngrid) )
+    allocate( V(ngrid, ngrid) )
+
+    X = linspace(-xlen/2, xlen/2, ngrid)
+    Y = linspace(-ylen/2, ylen/2, ngrid)
+    print *,X(:)
+
+end program main
