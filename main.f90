2aa708809b75282c1bf9e554314d35d5b6516ebd
greatcactus <kaiinntourokuyouno.1@gmail.com>
2021-05-04 22:28:26 +0900

    ADD: added subroutine to output the vts files.

--- main.f90
+++ main.f90
@@ -7,24 +7,26 @@ program main
     integer:: max_gen, gen, print_gen
     real(8):: xlen, ylen
     real(8):: size, Thot
+    real(8):: d
     character(16):: filename
-    integer, parameter:: lout=125
+    integer, parameter:: lout=125, lvtk=126
     real(8), allocatable, dimension(:,:):: U, V, Unew, Vnew
     real(8), allocatable, dimension(:)  :: X, Y
 
-    xgrid = 10000
+    xgrid = 500
     ygrid = xgrid
     Tin  = 1.39926
     fuel = 0.93781
-    dt = 0.00001
-    max_gen = 2000
-    print_gen = 10
+    dt = 0.000001
+    max_gen = 10000
+    print_gen = 100
     gen = 0
     t = gen*dt
     xlen = 10.
     ylen = xlen
     size = 0.25
     Thot = Tin*3.
+    d = 4.5
     filename = 'out.dat'
 
     allocate( X(xgrid) )
@@ -38,23 +40,28 @@ program main
     Y = linspace(-ylen/2, ylen/2, ygrid)
     call initialize_uv(U, V, Tin, fuel)
     call make_hotspot(U, X, Y, size, Thot)
-    open(lout, file=filename, form='unformatted')
-    write(lout) t
-    write(lout) U
-    write(lout) V
-    write(lout) X
-    write(lout) Y
-    close(lout)
+    !open(lout, file=filename, form='unformatted')
+    !write(lout) t
+    !write(lout) U
+    !write(lout) V
+    !write(lout) X
+    !write(lout) Y
+    !close(lout)
+    call out_vtk(gen, U, X, Y, lvtk)
 
     do while ( gen <= max_gen )
         gen = gen + 1
         t = gen*dt
-        call timestep(U, V, X, Y, dt, fuel, Unew, Vnew)
+
+        call timestep(U, V, X, Y, d, dt, fuel, Unew, Vnew)
         call copy_2darr(Unew, U)
         call copy_2darr(Vnew, V)
         if ( mod(gen, print_gen) == 0 ) then
-            call output(U, V, X, Y, t, filename, lout)
+            write(*,*) "step =>",gen
+            !call output(U, V, X, Y, t, filename, lout)
+            call out_vtk(gen, U, X, Y, lvtk)
         end if
+
     end do
 
     stop
@@ -91,11 +98,12 @@ subroutine make_hotspot(U, X, Y, size, Thot)
     return
 end subroutine make_hotspot
 
-subroutine timestep(U, V, X, Y, dt, fuel, Unew, Vnew)
+subroutine timestep(U, V, X, Y, d, dt, fuel, Unew, Vnew)
     real(8):: U(:,:), V(:,:), Unew(:,:), Vnew(:,:), X(:), Y(:)
     real(8):: dt
     real(8):: dx, dy
     real(8):: fuel
+    real(8):: d
     integer:: i,j
 
     !.... Inner computation
@@ -107,7 +115,7 @@ subroutine timestep(U, V, X, Y, dt, fuel, Unew, Vnew)
 &                       *(laplacian(U(i-1,j), U(i,j), U(i+1,j), U(i,j-1), U(i,j+1), dx, dy)&
 &                      + f(U(i,j), V(i,j)) )
             Vnew(i, j) = V(i,j) + dt&
-&                       *(laplacian(V(i-1,j), V(i,j), V(i+1,j), V(i,j-1), V(i,j+1), dx, dy)&
+&                      *(d*laplacian(V(i-1,j), V(i,j), V(i+1,j), V(i,j-1), V(i,j+1), dx, dy)&
 &                      + g(U(i,j), V(i,j)) )
         enddo
     enddo
@@ -135,8 +143,6 @@ subroutine output(U, V, X, Y, t, filename, lout)
     character(16):: filename
     integer, intent(in):: lout
 
-    !open(lout, file=filename, form='unformatted', action='write',&
-    !    &access='stream', status='old', iostat=ios, position='append')
     open(lout, file=filename, form='unformatted', position='append')
 
     write(lout) t
@@ -148,5 +154,43 @@ subroutine output(U, V, X, Y, t, filename, lout)
 
     return
 end subroutine output
+
+subroutine out_vtk(step, U, X, Y, lvtk)
+    integer, intent(in):: step
+    integer, intent(in):: lvtk
+    real(8), intent(in):: U(:,:)
+    real(8), intent(in):: X(:), Y(:)
+    character(40):: filename
+    integer:: i,j
+
+    write(filename, "(a,i5.5,a)") "data/temp",int(step),".vtk"
+    open(lvtk, file=filename)
+    write(lvtk, "('# vtk DataFile Version 3.0')")
+    write(lvtk, "('test')")
+    write(lvtk, "('ASCII ')")
+
+    !.... Axis section
+    write(lvtk, "('DATASET STRUCTURED_GRID')")
+    write(lvtk, "('DIMENSIONS ', 3(1x,i5))") ubound(X,1), ubound(X,1), 1
+    write(lvtk, "('POINTS ',i9,' float')") ubound(X,1)*ubound(Y,1)
+    do i=1, ubound(X, 1)
+        do j=1, ubound(Y, 1)
+            write(lvtk, *) X(i), Y(j), 0.0
+        end do
+    end do
+
+    !.... Data section
+    write(lvtk, "('POINT_DATA ', i9)") ubound(U, 1) * ubound(U, 2)
+    write(lvtk, "('SCALARS temp float')")
+    write(lvtk, "('LOOKUP_TABLE default')")
+    do i=1, ubound(U, 1)
+        do j=1, ubound(U, 2)
+            write(lvtk,*) U(i,j)
+        end do
+    end do
+    close(lvtk)
+
+    return
+end subroutine out_vtk
 
 end program main
